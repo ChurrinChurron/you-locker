469a2371235b79d689d4c15dd4900595eba5566a
ChurrinChurron <marianafantasy8@gmail.com>
2024-03-11 17:47:33 -0400

    funcion formulario

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ let btnNav = document.querySelector("#btnMov");
 let navMov = document.querySelector(".nav__movil");
 let cerrBtnNav = document.querySelector("#cerrarBtnMov");
 let navMovLinks = document.querySelectorAll(".movil__links > a");
+let formTexfile=document.getElementById('cotizar');
+
+// Función para abrir y cerrar el nav movil
 
 btnNav.addEventListener("click", () => {
     navMov.classList.add("abrir");
@@ -17,4 +20,23 @@ navMovLinks.forEach(links => {
 
         navMov.classList.remove("abrir");
     })
+})
+
+// Función para cotizar 
+
+let urlWhat=[];
+
+formTexfile.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
+    let inputs = document.querySelectorAll("input");
+
+    inputs.forEach(function(input) {
+        urlWhat.push(input.value)
+    });
+
+      //573244800000
+    let message = encodeURIComponent(`Hola *YouLocker*\nQuiero cotizar el envío de mi paquetería\nPeso: ${urlWhat[0]}\nDetalles: ${urlWhat[1]}\nEmail: ${urlWhat[2]}`);
+    
+    window.location.href = `https://wa.me/573244800000?text=${message}`;
 })
